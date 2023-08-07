09280fb8265e9a6463f82ac5d3363c50410eb6d3
clocs <dascolin@gmail.com>
2023-08-07 13:14:43 -0400

    extracted datetime from roam filename; generated hepta filename

--- heptabase/convert_roam_journal_to_hepta.py
+++ heptabase/convert_roam_journal_to_hepta.py
@@ -1,12 +1,80 @@
-import os
 import argparse
+import datetime
+import os
+import re
+import pandas as pd
+import shutil
+from typing import Optional
+
+
+MONTH_NUMBER_DICT = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+
+def extract_date_time(filename: str) -> Optional[datetime.datetime]:
+    datetime_out = None
+    regex_str = '(\w+)\s([0-9]{1,2})[a-z]{2}[,]\s([0-9]{4}).md'
+    re_date_elems = re.search(regex_str, filename)
+    if re_date_elems:
+        month_num = MONTH_NUMBER_DICT[re_date_elems.group(1)]
+        datetime_out = datetime.date(int(re_date_elems.group(3)),
+                                     int(month_num),
+                                     int(re_date_elems.group(2)))
+    return datetime_out
+
+
+def hepta_name_from_date(date: datetime) -> str:
+    if date:
+        ymd_format = date.isoformat()
+        hepta_format = ymd_format + ".md"
+    else:
+        hepta_format = ""
+    return hepta_format
+
 
 def convert_roam_journal_to_hepta(dir_roam_export: str) -> None:
     # Get list of journal files
-    billy = 1
+    objs = os.listdir(dir_roam_export)
+    files = [o for o in objs
+             if os.path.isfile(os.path.join(dir_roam_export, o))]
+    regex_str = '\w+\s[0-9]{1,2}[a-z]{2}[,]\s[0-9]{4}.md'
+    r = re.compile(regex_str)
+    j_files = list(filter(r.match, files))
+
     # Create output directory
+    parent_path, dir_name = os.path.split(dir_roam_export)
+    out_dir_name = dir_name + '-converted'
+    out_dir_path = os.path.join(parent_path, out_dir_name)
+    # if os.path.exists(out_dir_path):
+    #     print(f"Warning: converted files already exist. "
+    #           f"Please delete dir: {out_dir_path}. Then re-run.")
+    #     exit(0)
+    # os.makedirs(out_dir_path)
+
     # Copy Roam journal files to output directory
+    df = pd.DataFrame({'roam_filename': j_files})
+    df['roam_filepath'] = df['roam_filename'].apply(
+        lambda f: os.path.join(dir_roam_export, f))
+
+    # Extract date and generate new names
+    df['datetime'] = df['roam_filename'].apply(extract_date_time)
+    df['hepta_filename'] = df['datetime'].apply(hepta_name_from_date)
+
     # Rename Roam journal files to Heptabase file format
+    billy = 1
+
     pass
 
 
@@ -16,13 +84,6 @@ def parse_args():
                         help='Path to exported Roam MD files, unzipped',
                         required=True,
                         type=str)
-    # parser.add_argument("--output_dir", '-o',
-    #                     help='Path to output converted journal files. '
-    #                          'If none, script generates a folder next to '
-    #                          'input folder.',
-    #                     required=False,
-    #                     default='',
-    #                     type=str)
     return vars(parser.parse_args())
 
 
